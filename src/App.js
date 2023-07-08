0c0e6ceb57abba49d7bd86a53feba8c5eb772a24
K4R7IK <kartik.workspace@gmail.com>
2023-07-09 03:00:28 +0530

    Added Login Fucntionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Home from "./container/Home.jsx";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
